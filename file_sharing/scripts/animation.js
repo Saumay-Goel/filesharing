febc061e57245297f14c545b1e74fde9d88df2d2
i will learn a lot <144949928+SaumayGoel@users.noreply.github.com>
2024-01-11 23:24:20 +0530

    Add files via upload

---
+++ file_sharing/scripts/animation.js
@@ -0,0 +1,43 @@
+const file_icon = document.querySelector(".file_icon");
+const file_icon_y = document.querySelector("#file_icon_y");
+const file_icon_x = document.querySelector(".file_icon_x");
+const box_lid_x = document.querySelector(".box_lid_x");
+const ufo_y = document.querySelector(".ufo_y")
+const ufo = document.querySelector(".ufo")
+const ufo_first_y = document.querySelector(".ufo_first_y")
+const ufo_main_x = document.querySelector(".ufo_main_x")
+const ufo_main_y = document.querySelector(".ufo_main_y")
+const ufo_light= document.querySelector(".ufo_light")
+const move_whole_body = document.querySelector(".move_whole_body")
+const box_lid = document.querySelector(".box_lid")
+const box_body = document.querySelector(".box_body")
+
+let amt = () => {
+    //file_icon.classList.toggle("show");
+    ufo_light.classList.add('ufo_light_prop')
+    file_icon.classList.add("animate_file_icon_size");
+    file_icon_y.classList.add("animate_file_icon_y");
+    file_icon_x.classList.add("animate_file_icon_x");
+    box_lid_x.classList.add("animate_box_lid_x");
+    ufo.classList.add("animate_ufo_hover");
+    ufo_y.classList.add("animate_ufo_hover_y");
+    ufo_first_y.classList.add("animate_ufo_first_y")
+    ufo_first_y.addEventListener('animationend',()=>{
+        ufo_main_x.classList.add("animate_ufo_x")
+        ufo_main_y.classList.add("animate_ufo_y")
+    })
+    ufo_light.classList.add('ufo_light_animation')
+    move_whole_body.classList.add('move_whole_body_animate');
+    box_lid.classList.add('move_whole_body_shrink_animate');
+    box_body.classList.add('move_whole_body_shrink_animate');
+
+    //ufo_y.classList.add("animate_ufo_first_y");
+    //     file_icon.classList.remove("animate_file_icon_size")
+    //     file_icon_y.classList.remove("animate_file_icon_y")
+    //     file_icon_x.classList.remove("animate_file_icon_x")
+    //})
+}
+
+
+
+export { amt };
